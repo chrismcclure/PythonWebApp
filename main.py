aeac06e1f9d0aa5ba36ead78fc23378eabc28493
Chris McClure <CMcClure@penlink.com>
2022-04-02 10:10:40 -0500

    feat: add favicon and run on port 0.0.0.0

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5001)
+    # This so I could run it on other computers in my network
+    # will probably remove this
